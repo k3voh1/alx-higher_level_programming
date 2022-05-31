788e5ca941f0f23304684e93ffb2b74cb9fb0861
k3voh1 <99652522+k3voh1@users.noreply.github.com>
2022-05-31 19:31:26 +0100

    task 1 corr

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ number_string = repr(number)
 lastDigitStr = number_string[-1]
 lastDigit = int(lastDigitStr)
 if number > 0:
-    print("Last digit of", number, "is", lastDigit, end=" ")
+    print("last digit of", number, "is", lastDigit, end=" ")
     if lastDigit > 5:
         print("and is greater than 5")
     elif lastDigit == 0:
@@ -13,7 +13,8 @@ if number > 0:
     else:
         print("and is less than 6 and not 0")
 else:
-    print("Last digit of", number, "is", -lastDigit, end=" ")
+    lastDigit = lastDigit * -1
+    print("last digit of", number, "is", lastDigit, end=" ")
     if lastDigit == 0:
         print("and is 0")
     else:
